88d8e72cf9b2bfdda835ce03b95ef18f10d98180
Ekta <18075@arsd.du.ac.in>
2023-07-26 20:57:07 +0530

    Zero One Pyramid pattern

---
+++ zeroonepyramid.java
@@ -0,0 +1,18 @@
+public class zeroonepyramid {
+    public static void zeroonePattern(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        zeroonePattern(8);
+    }
+}
